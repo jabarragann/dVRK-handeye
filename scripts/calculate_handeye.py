51bcddc966a457d144fdfd21b9a5fba8e0b54632
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-12-28 23:04:37 -0500

    Store handeye in hdf5 file

--- scripts/calculate_handeye.py
+++ scripts/calculate_handeye.py
@@ -16,6 +16,15 @@ def main():
     print("Y_est_check:\n", Y_est_check)
     print("error_stats:\n", error_stats)
 
+    with h5py.File("data/collection1.hdf5", "a") as hdf_file:
+        handeye_group = hdf_file.create_group("hand_eye_calibration")
+        handeye_group.create_dataset("X", data=X)
+        handeye_group.create_dataset("Y", data=Y)
+
+        handeye_group.create_dataset("Y_est_check", data=Y_est_check)
+        handeye_group.attrs["mean_error"] = error_stats[0, 0]
+        handeye_group.attrs["std_error"] = error_stats[1, 0]
+
 
 if __name__ == "__main__":
     main()
