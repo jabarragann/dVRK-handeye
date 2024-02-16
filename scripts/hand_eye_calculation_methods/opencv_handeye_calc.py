a14989789c8ea310df4ca7204636b2bb4401e318
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-02-16 12:32:23 -0500

    hand-eye calib debug script [not working yet]

--- scripts/hand_eye_calculation_methods/opencv_handeye_calc.py
+++ scripts/hand_eye_calculation_methods/opencv_handeye_calc.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pathlib import Path
 import cv2
 from dvrk_handeye.VisionTracker import VisionTracker
+from cv2 import aruco
 
 
 # fmt: off
@@ -19,13 +20,19 @@ def load_images_data(root_path: Path, idx: int):
     return left_img
 
 
-def load_poses_data(root_path: Path):
+def load_poses_data(root_path: Path) -> np.ndarray:
     file_path = root_path / "pose_data.csv"
     pose_data = pd.read_csv(file_path).values
     pose_data = pose_data[:, 1:]  # remove index column
     pose_data = pose_data.reshape(-1, 4, 4)
 
     return pose_data
+
+
+def to_homogeneous(rot: np.ndarray, t: np.ndarray) -> np.ndarray:
+    T = np.concatenate([rot, t], axis=1)
+    T = np.vstack([T, [0, 0, 0, 1]])
+    return T
 
 
 def main():
@@ -35,9 +42,23 @@ def main():
 
     img_path = root_path / "imgs" / "left"
     total_images = len(list(img_path.glob("*.jpeg")))
+
+    robot_poses = load_poses_data(root_path)
+
     print(f"Total images: {total_images}")
+    print(f"Total poses: {robot_poses.shape[0]}")
 
     last_img_to_use = 102
+
+    cam_T_marker_rot = []
+    cam_T_marker_t = []
+    marker_T_cam_rot = []
+    marker_T_cam_t = []
+    gripper_T_base_rot = []
+    gripper_T_base_t = []
+    base_T_gripper_rot = []
+    base_T_gripper_t = []
+    imgs = []
     for i in range(last_img_to_use):
         img = load_images_data(root_path, i)
 
@@ -45,27 +66,86 @@ def main():
 
         if len(corners) > 0:
             print(f"Image {i}: Found markers")
+            rvec, tvec, marker_points = vision_tracker.estimate_pose(corners[0])
+            marker_R = cv2.Rodrigues(rvec)[0]
+            
+            imgs.append(img)
+
+            cam_T_marker = to_homogeneous(marker_R, tvec[0].T)
+            cam_T_marker_rot.append(marker_R)
+            cam_T_marker_t.append(tvec)
+
+            marker_T_cam = np.linalg.inv(cam_T_marker)
+            marker_T_cam_rot.append(marker_T_cam[:3, :3])
+            marker_T_cam_t.append(marker_T_cam[:3, 3])
+
+            gripper_T_base = np.linalg.inv(robot_poses[i])
+            gripper_T_base_rot.append(gripper_T_base[:3, :3])
+            gripper_T_base_t.append(gripper_T_base[:3, 3])
+
+            base_T_gripper_rot.append(robot_poses[i][:3, :3])
+            base_T_gripper_t.append(robot_poses[i][:3, 3])
+
         else:
             print(f"Image {i}: No markers found")
 
-    # idx = 30
-    # img = load_images_data(root_path, idx)
+    base_T_cam_rot, base_T_cam_t = cv2.calibrateHandEye(
+        gripper_T_base_rot,
+        gripper_T_base_t,
+        cam_T_marker_rot,
+        cam_T_marker_t,
+        method=cv2.CALIB_HAND_EYE_HORAUD,
+    )
 
-    # pose_data = load_poses_data(root_path)
+    gripper_T_marker_rot, gripper_T_marker_t = cv2.calibrateHandEye(
+        base_T_gripper_rot,
+        base_T_gripper_t,
+        marker_T_cam_rot,
+        marker_T_cam_t,
+        method=cv2.CALIB_HAND_EYE_HORAUD,
+    )
 
-    # pose = Y_inv @ pose_data[idx]
-    # tvec = pose[:3, 3]
-    # rvec = cv2.Rodrigues(pose[:3, :3])[0]
+    print(f"Base_T_Cam_Rot {base_T_cam_rot}")
+    print(f"Base_T_Cam_t {base_T_cam_t}")
+    print(f"dist between base and cam {np.linalg.norm(base_T_cam_t)}")
 
-    # points_3d = np.array([[[0, 0, 0]]], np.float32)
-    # points_2d, _ = cv2.projectPoints(points_3d, rvec, tvec, mtx, dist)
+    print("single image validation")
+    base_T_cam = to_homogeneous(base_T_cam_rot, base_T_cam_t)
+    gripper_T_marker = to_homogeneous(
+        gripper_T_marker_rot, gripper_T_marker_t
+    )
 
-    # print(points_2d)
-    # print(pose_data[idx])
+    print(f"base_T_cam \n{base_T_cam}")
+    print(f"gripper_T_marker \n{gripper_T_marker}")
 
-    # cv2.imshow("img", img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    cam_T_base = np.linalg.inv(base_T_cam)
+
+    idx = 30
+    test_img = load_images_data(root_path, idx)
+    base_T_gripper = robot_poses[idx]
+
+    # marker
+    corners, ids, rejectedImgPoints = vision_tracker.detect_markers(img)
+    rvec, tvec, marker_points = vision_tracker.estimate_pose(corners[0])
+    rvec = cv2.Rodrigues(rvec)[0]
+    cam_T_marker = to_homogeneous(rvec, tvec[0].T)
+
+    # calc gripper_T_marker
+    gripper_T_marker_2 = gripper_T_base @ base_T_cam @ cam_T_marker
+    print(f"gripper_T_marker_2 \n{gripper_T_marker_2}")
+
+    # The nonsense section
+    pose = cam_T_base @ base_T_gripper
+    tvec = pose[:3, 3]
+    rvec = cv2.Rodrigues(pose[:3, :3])[0]
+    points_3d = np.array([[[0, 0, 0]]], np.float32)
+    points_2d, _ = cv2.projectPoints(points_3d, rvec, tvec, mtx, dist)
+
+    print(points_2d)
+
+    cv2.imshow("img", test_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
